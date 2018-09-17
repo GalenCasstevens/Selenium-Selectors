d4259f47e85c10f51dc096d82fc190f7567562b6
Galen Casstevens <Galen.Casstevens@agdata.com>
2018-09-17 16:18:52 -0400

    }

---
+++ IDSelector/EntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        string url = "http://testing.todorvachev.com/selectors/id/",
+               id = "testImage";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.Id(id));
+
+        if (element.Displayed)
+        {
+            GreenMessage("Yes, I can see it!");
+        }
+        else
+        {
+            RedMessage("Nope, it's not there!");
+        }
+
+        driver.Close();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
